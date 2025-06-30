9b1e9404dc79cfcaaafc95fed64b0cf6d63aba71
Delocalized Bond <editor@delocalized.bond>
2025-06-30 18:36:52 +0400

    nala should be correctly set now

--- install/desktop/app-wl-clipboard.sh
+++ install/desktop/app-wl-clipboard.sh
@@ -1,9 +1,8 @@
 # Provides a system clipboard interface for Neovim under Wayland
 
 # Install wl-clipboard (Wayland clipboard tool)
-if [ "$OMAKUB_OS_ID" = "debian" ]; then
-  sudo dpkg -i wl-clipboard.deb
-  sudo apt-get install -f
+if  [ "$OMAKUB_OS_ID" = "debian" ]; then
+  sudo $INSTALLER install wl-clipboard
 else
   echo "Unsupported OS for wl-clipboard installation."
   exit 1
