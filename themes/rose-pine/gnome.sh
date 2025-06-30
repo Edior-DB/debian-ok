b7e46b650abb4a54378dc5738afba6d3e7c3d9d6
Delocalized Bond <editor@delocalized.bond>
2025-06-30 18:44:48 +0400

    env var compatibility

--- themes/rose-pine/gnome.sh
+++ themes/rose-pine/gnome.sh
@@ -1,5 +1,6 @@
 OMAKUB_THEME_COLOR="red"
 DEBIANOK_THEME_COLOR="$OMAKUB_THEME_COLOR"
 OMAKUB_THEME_BACKGROUND="rose-pine/background.jpg"
+DEBIANOK_THEME_BACKGROUND="$OMAKUB_THEME_BACKGROUND"
 source $OMAKUB_PATH/themes/set-gnome-theme.sh
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
