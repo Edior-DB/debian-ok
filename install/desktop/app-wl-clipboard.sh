abac417412fdc7db1ac093ae91f69c04876fd161
Delocalized Bond <editor@delocalized.bond>
2025-06-30 18:33:34 +0400

    nala should be correctly set now

--- install/desktop/app-wl-clipboard.sh
+++ install/desktop/app-wl-clipboard.sh
@@ -2,7 +2,7 @@
 
 # Install wl-clipboard (Wayland clipboard tool)
 if [ "$OMAKUB_OS_ID" = "debian" ]; then
-  sudo dpkg -i wl-clipboard*.deb
+  sudo dpkg -i wl-clipboard.deb
   sudo apt-get install -f
 else
   echo "Unsupported OS for wl-clipboard installation."
