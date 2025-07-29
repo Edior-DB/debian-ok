776ef7595c69e92d9e364b02b8aa236994aa51bb
Delocalized Bond <editor@delocalized.bond>
2025-07-29 22:26:58 +0400

    preparations for Debian 13

--- install/desktop/required/app-alacritty.sh
+++ install/desktop/required/app-alacritty.sh
@@ -6,11 +6,12 @@ mkdir -p ~/.config/alacritty
 
 
 # Fetch visuals (theme, font) from Chris Titus, but keep your own keybinds
-curl -sSLo ~/.config/alacritty/alacritty.toml "https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/alacritty/alacritty.toml"
-curl -sSLo ~/.config/alacritty/nordic.toml "https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/alacritty/nordic.toml"
+#curl -sSLo ~/.config/alacritty/alacritty.toml "https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/alacritty/alacritty.toml"
+curl -sSLo ~/.config/alacritty/theme.toml "https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/alacritty/nordic.toml"
 curl -sSLo ~/.config/alacritty/keybinds.toml "https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/alacritty/keybinds.toml"
 
 # Retain other custom settings
+cp ~/.local/share/debian-ok/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
 cp ~/.local/share/debian-ok/configs/alacritty/shared.toml ~/.config/alacritty/shared.toml
 cp ~/.local/share/debian-ok/configs/alacritty/pane.toml ~/.config/alacritty/pane.toml
 cp ~/.local/share/debian-ok/configs/alacritty/btop.toml ~/.config/alacritty/btop.toml
