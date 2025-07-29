302ba871272e72f8ba8640c8a9d74646a38ed762
Delocalized Bond <editor@delocalized.bond>
2025-07-29 20:28:24 +0400

    chore: update font and font-size scripts in bin/debianok-sub

--- bin/debianok-sub/font-size.sh
+++ bin/debianok-sub/font-size.sh
@@ -1,4 +1,4 @@
-choice=$(gum choose {7..14} "<< Back" --height 11 --header "Choose your terminal font size")
+choice=$(gum choose {10..18} "<< Back" --height 11 --header "Choose your terminal font size")
 
 if [[ $choice =~ ^[0-9]+$ ]]; then
 	# Use portable sed for both GNU and BSD/macOS
